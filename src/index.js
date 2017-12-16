15503a538ff3f7887523e20832c808abe026f732
rahid <hidayat.85@gmail.com>
2017-12-16 11:59:12 +0700

    setting router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDODM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/App'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 
 ReactDODM.render(
-  <div>App</div>, document.getElementById('root')
+  <BrowserRouter>
+    <div>
+      <Route path ="/app" component={App} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+    </div>
+  </BrowserRouter>
+  , document.getElementById('root')
 )
